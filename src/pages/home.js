810d334f7feab73bb3c2f35d66876666a42db74f
梁芳芳 <liangfangfang@arcblock.io>
2022-06-13 20:35:51 +0800

    fix(17)-guestOrOther no permission to post

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,12 +6,16 @@ import Editor from '../components/editors';
 import Feeds from '../components/feeds';
 
 import { PostProvider } from '../contexts/post';
+import { useSessionContext } from '../contexts/session';
 
 const Home = () => {
+  const { session } = useSessionContext();
+  window.console.log(session.user.role);
+
   return (
     <Div>
       <PostProvider pageSize={10}>
-        <Editor />
+        {session.user.role !== 'guest' && session.user.role !== 'other' && <Editor />}
         <Feeds />
       </PostProvider>
     </Div>
